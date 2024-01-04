bebb929e0601105223fa386ea0d18a5354fe1ebe
Anthony Shellman <atshellman@gmail.com>
2024-01-04 15:18:28 -0700

    Import and Render Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from './components/Header/Header'
 import './App.css'
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-    <h1>Hello!</h1>
+    <Header/>
     </>
   )
 }
