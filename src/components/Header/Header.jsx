775fdf2b88e9a7dd209aff0199f5dc3cf22ba3d2
Anthony Shellman <atshellman@gmail.com>
2024-01-04 15:19:44 -0700

    Import scss file and render h1

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.scss'
+
+const Header = () => {
+
+  return (
+    <header>
+      <h1 className="intro-text">
+        We make your music sound extraordinary.
+      </h1>
+    </header>
+  )
+}
+
+export default Header
