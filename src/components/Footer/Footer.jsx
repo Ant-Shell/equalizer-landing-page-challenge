43df338e11453548b156f8932d8a307c3a9e3ae0
Anthony Shellman <atshellman@gmail.com>
2024-01-08 14:40:54 -0700

    Add initial outline of Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import './Footer.scss'
+import logo from '../../assets/logo.svg'
+import facebookLogo from '../../assets/icon-facebook.svg'
+import instagramLogo from '../../assets/icon-instagram.svg'
+import twitterLogo from '../../assets/icon-twitter.svg'
+
+const Footer = () => {
+
+  return (
+    <footer className='footer'>
+      <img className='footer-logo' src={logo} alt='Logo'/>
+      <p className='footer-message'>
+        All rights reserved © Equalizer 2021
+        Have any problems? Contact us via social media or email us at equalizer@example.com
+      </p>
+      <div className='footer-logo-container'>
+        <img className='footer-facebook-logo' src={facebookLogo} alt='Facebook logo'/>
+        <img className='footer-instagram-logo' src={instagramLogo} alt='Instagram logo'/>
+        <img className='footer-twitter-logo' src={twitterLogo} alt='Twitter Logo'/>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
