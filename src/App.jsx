411b7cd388131c327a99e9423c233fd695cc274f
Anthony Shellman <atshellman@gmail.com>
2024-01-08 14:38:56 -0700

    Import Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Footer from './components/Footer/Footer'
 import './styles/_global.scss'
 import './App.scss'
 
 function App() {
   
-
   return (
     <>
       <Header/>
       <Hero/>
+      <Footer/>
     </>
   )
 }
