812c87b8da46b44d2b241c864a2428abf6ce5d5d
Anthony Shellman <atshellman@gmail.com>
2024-01-08 11:20:59 -0700

    Add cta-content-container

--- src/components/CallToActionCard/CallToActionCard.jsx
+++ src/components/CallToActionCard/CallToActionCard.jsx
@@ -4,18 +4,20 @@ const CallToActionCard = () => {
 
   return (
     <section className='cta'>
-      <h2 className='cta-header-text'>
-        Premium EQ
-      </h2>
-      <p className='cta-info-text'>
-        Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take 
-        your listening experience to a whole new level and access all our incredible features!
-      </p>
-      <p className='cta-pricing-text'>
-        $4 / month
-      </p>
-      <button className='cta-ios-button'>iOS Download</button>
-      <button className='cta-android-button'>Android Download</button>
+      <div className='cta-content-container'>
+        <h2 className='cta-header-text'>
+          Premium EQ
+        </h2>
+        <p className='cta-info-text'>
+          Get expert-level control with a robust equalizer, volume mixer, and spatial audio. Take
+          your listening experience to a whole new level and access all our incredible features!
+        </p>
+        <p className='cta-pricing-text'>
+          <span className='cta-pricing-bold'>$4</span><span>/ month</span>
+        </p>
+        <button className='cta-ios-button'>iOS Download</button>
+        <button className='cta-android-button'>Android Download</button>
+      </div>
     </section>
   )
 }
