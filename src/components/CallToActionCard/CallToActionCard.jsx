496c28a35f5d720376772ff6e66d162f1bdbc138
Anthony Shellman <atshellman@gmail.com>
2024-01-08 13:29:17 -0700

    Add icons and initial styling

--- src/components/CallToActionCard/CallToActionCard.jsx
+++ src/components/CallToActionCard/CallToActionCard.jsx
@@ -1,4 +1,6 @@
 import './CallToActionCard.scss'
+import androidIcon from '../../assets/icon-android.svg'
+import appleIcon from '../../assets/icon-apple.svg'
 
 const CallToActionCard = () => {
 
@@ -16,8 +18,14 @@ const CallToActionCard = () => {
           <span className='cta-pricing-bold'>$4</span>
           <span className='cta-per-month'> / month</span>
         </p>
-        <button className='cta-ios-button'>iOS Download</button>
-        <button className='cta-android-button'>Android Download</button>
+        <button className='cta-ios-button'>
+          <img className='cta-apple-icon' src={appleIcon} alt='Apple icon'/>
+          <span>iOS Download</span>
+        </button>
+        <button className='cta-android-button'>
+          <img className='cta-android-icon' src={androidIcon} alt='Android icon'/>
+          <span>Android Download</span>
+        </button>
       </div>
     </section>
   )
